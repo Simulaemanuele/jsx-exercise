66e9a78936956625b9cff335405c83b6bd52769d
Emanuele Simula <simulaemanuele@gmail.com>
2022-07-16 09:57:44 +0200

    exercise: jsx-02 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { HelloWorld } from "./HelloWorld";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
     render(){
         return <div>
             <h1>JSX-EXERCISES</h1>
 
+            <Welcome name='Annibale'/>
         </div>
     }
 }
